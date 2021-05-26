59cb1b95492754de2b5a7f4a9ede524ba7fe23ef
강경훈 <kyunghun@gang-gyeonghuns-MacBook-Pro.local>
2021-05-26 17:28:40 +0900

    first commit

---
+++ book/tests.py
@@ -0,0 +1,121 @@
+import json
+
+import bcrypt
+
+from .models        import Book, Author, AuthorBook, User
+from django.test    import TestCase
+from django.test    import Client
+from unittest.mock  import patch, MagicMock
+
+class AuthorTest(TestCase):
+    def setUp(self):
+        Author.objects.create(
+            name  = 'Brendan Eich',
+            email = 'BrendanEich@gmail.com'
+        )
+
+    def tearDown(self):
+        Author.objects.all().delete()
+
+    def test_authorkview_post_success(self):
+        client = Client()
+        author = {
+            'name'  : 'Guido van Rossum',
+            'email' : 'GuidovanRossum@gmail.com'
+        }
+        response = client.post('/book/author', json.dumps(author), content_type='application/json')
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_authorkview_post_duplicated_name(self):
+        client = Client()
+        author = {
+            'name'  : 'Brendan Eich',
+            'email' : 'GuidovanRossum@gmail.com'
+        }
+        response = client.post('/book/author', json.dumps(author), content_type='application/json')
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(),
+            {
+                'message' : 'DUPLICATED_NAME'
+            }
+        )
+
+    def test_authorkview_post_invalid_keys(self):
+        client = Client()
+        author = {
+            'first_name'  : 'Guido van Rossum',
+            'email'       : 'GuidovanRossum@gmail.com'
+        }
+        response = client.post('/book/author', json.dumps(author), content_type='application/json')
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(),
+            {
+                'message':'INVALID_KEYS'
+            }
+        )
+
+class AuthorBookTest(TestCase):
+    def setUp(self):
+        client = Client()
+        Book.objects.create(
+            id    = 1,
+            title = 'python'
+        )
+
+        Book.objects.create(
+            id    = 2,
+            title = 'javascript'
+        )
+
+        Author.objects.create(
+            id    = 1,
+            name  = 'Guido van Rossum',
+            email = 'GuidovanRossum@gmail.com'
+        )
+
+        Author.objects.create(
+            id    = 2,
+            name  = 'Brendan Eich',
+            email = 'BrendanEich@gmail.com'
+        )
+
+        AuthorBook.objects.create(
+            book   = Book.objects.get(id=1),
+            author = Author.objects.get(id=1)
+        )
+
+        AuthorBook.objects.create(
+            book   = Book.objects.get(id=1),
+            author = Author.objects.get(id=2)
+        )
+
+        AuthorBook.objects.create(
+            book   = Book.objects.get(id=2),
+            author = Author.objects.get(id=1)
+        )
+
+        AuthorBook.objects.create(
+            book   = Book.objects.get(id=2),
+            author = Author.objects.get(id=2)
+        )
+
+    def tearDown(self):
+        Book.objects.all().delete()
+        Author.objects.all().delete()
+        AuthorBook.objects.all().delete()
+
+    def test_authorbook_get_success(self):
+        client   = Client()
+        response = client.get('/book/author-book/1')
+        self.assertEqual(response.json(),
+            {
+                "authors" : [
+                    {'author': 1}, 
+                    {'author': 2}
+                ]    
+            }
+        )
+        self.assertEqual(response.status_code, 200)  
---
+++ book/views.py
@@ -0,0 +1,36 @@
+import json
+import jwt
+import requests
+from django.views import View
+from django.http  import JsonResponse, HttpResponse
+
+from .models      import Book, Author, AuthorBook, User
+
+class AuthorView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        try:
+            if Author.objects.filter(name = data["name"]).exists():
+                return JsonResponse({'message':'DUPLICATED_NAME'}, status = 400)
+            
+            Author(
+                name  = data['name'],
+                email = data['email']
+            ).save()
+            return HttpResponse(status=200)
+
+        except KeyError:
+            return JsonResponse({'message':'INVALID_KEYS'}, status = 400)
+
+class AuthorBookView(View):
+    def get(self, request, book_id):
+        try:
+            if Book.objects.filter(id = book_id).exists():
+                book    = Book.objects.get(id = book_id)
+                authors = list(AuthorBook.objects.filter(book = book).values('author'))
+                return JsonResponse({'authors':authors}, status = 200)
+            
+            return JsonResponse({'message':'NO_AUTHOR'}, status = 400)
+        except KeyError:
+            return JsonResponse({'message':'INVALID_KEYS'}, status = 400)
+
